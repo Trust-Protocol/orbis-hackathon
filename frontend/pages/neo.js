ddbbe9d0de87d98f343ac9d66a3b4cac31d5db0d
LyghtCode <xikago.musica@gmail.com>
2022-12-29 12:25:36 -0500

    Update neo.js

--- frontend/pages/neo.js
+++ frontend/pages/neo.js
@@ -42,10 +42,35 @@ export default function Neo() {
 		// Instantiate PassportScorer
 		const scorer = new PassportScorer([
 			{
+				provider: "BrightID",
+				issuer: "did:key:z6MkghvGHLobLEdj1bgRLhS4LPGJAvbMA1tn2zcRyqmYU5LC",
+				score: 0.5
+			},
+			{
 				provider: "Github",
 				issuer: "did:key:z6MkghvGHLobLEdj1bgRLhS4LPGJAvbMA1tn2zcRyqmYU5LC",
 				score: 0.5
-			}
+			},
+			{
+				provider: "Twitter",
+				issuer: "did:key:z6MkghvGHLobLEdj1bgRLhS4LPGJAvbMA1tn2zcRyqmYU5LC",
+				score: 0.25
+			},
+			{
+				provider: "Discord",
+				issuer: "did:key:z6MkghvGHLobLEdj1bgRLhS4LPGJAvbMA1tn2zcRyqmYU5LC",
+				score: 0.25
+			},
+			{
+				provider: "POAP",
+				issuer: "did:key:z6MkghvGHLobLEdj1bgRLhS4LPGJAvbMA1tn2zcRyqmYU5LC",
+				score: 0.25
+			},
+			{
+				provider: "Linkedin",
+				issuer: "did:key:z6MkghvGHLobLEdj1bgRLhS4LPGJAvbMA1tn2zcRyqmYU5LC",
+				score: 0.25
+			},
 		]);
 
 		try {
